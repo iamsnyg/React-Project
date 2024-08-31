58238222de5bec22785aee7400c41680a1cb84d2
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-08-31 13:06:27 +0530

    chore: Update fonts and navigation bar styling

--- BuyPage/src/App.jsx
+++ BuyPage/src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import from './assets'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <nav>
         <div className="">
-          <img src="./assets/brand_logo.png" alt="logo" />
+          <img src="/images/brand_logo.png" alt="logo" />
+          {/* <img src="/src/assets/shoe_image.png" alt="shoe" /> */}
         </div>
 
         <ul>
           <li href="#">MENU</li>
-          <li href="#">LOCATION</li>
-          <li href="#">ABOUT</li>
+          <li href="#" className='font-Nerko One'>HOME</li>
+          {/* '>LOCATION</li> */}
+          <li href="#" 
+          className='font-display'>LOCATION</li>
+          {/* >ABOUT</li> */}
           <li href="#">CONTACT</li>
-        </ul>
+        </ul> 
 
-        <button className='bg-blue-400'  >login</button>
+         <button className='bg-blue-300 rounded-md p-2 shadow-md ' >hello</button>
+      
       </nav>
+
     </>
   )
 }
--- BuyPage/tailwind.config.js
+++ BuyPage/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Nerko': ['Nerko+One', 'cursive'],
+        'display': ['Matemasie', 'sans-serif'],
+      
+      }
+    },
   },
   plugins: [],
 }
