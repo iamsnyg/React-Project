2e6f1d3a7d00e506c353931c51471b80a880b148
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-09-03 01:54:34 +0530

    chore: Add Chakra UI and Framer Motion dependencies

--- cryptoDashboard/src/main.jsx
+++ cryptoDashboard/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ChakraBaseProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraBaseProvider>
+      <App />
+    </ChakraBaseProvider>
   </StrictMode>,
 )
