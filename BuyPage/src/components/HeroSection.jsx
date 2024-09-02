447dee3a081a3acb0bfbb071ce190033b8227f87
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-09-02 23:58:06 +0530

    chore: Update HeroSection component layout and styling

--- BuyPage/src/components/HeroSection.jsx
+++ BuyPage/src/components/HeroSection.jsx
@@ -3,20 +3,36 @@ import React from 'react'
 function HeroSection() {
   return (
     <>
-    <section>
-        <h1>
-            YOUR FEET DESERVE THE BEST
-        </h1>
+    <main >
+        <div className='flex max-w-screen-xl'>
+            <div>
+                <h1 className='
+                text-3xl font-bold text-gray-800
+                '>YOUR FEET DESERVE THE BEST</h1>
 
-        <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOE.
-        YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOE.
-        </p>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOE.
+                YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOE.</p>
 
-        <div>
-            <button>Shop now</button>
-            <button>Category  </button>
+                <div>
+                    <button>Shop now </button>
+                    <button>Category </button>
+                </div>
+
+                <div>
+                    <p>Also available on</p>
+                </div>
+                <div>
+                    <img src="/images/amazon-filpkat-project.png" alt="amazon" />
+                    
+                </div>
+
+            </div>
+            <div>
+                <img src="/src/assets/shoe_image.png" alt="shoe" />
+            </div>
         </div>
-    </section>
+
+    </main>
     </>
   )
 }
