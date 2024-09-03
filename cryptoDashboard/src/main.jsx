075a1be77ee40b52037aa0efc3fcc1410424310d
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-09-04 01:30:10 +0530

    chore: Update fonts and navigation bar styling

--- cryptoDashboard/src/main.jsx
+++ cryptoDashboard/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraBaseProvider } from '@chakra-ui/react'
+import "@fontsource/ubuntu";
+import { theme } from './theme/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraBaseProvider>
+    <ChakraBaseProvider theme={theme}>
       <App />
     </ChakraBaseProvider>
   </StrictMode>,
--- cryptoDashboard/src/pages/Dashboard/Dashboard.jsx
+++ cryptoDashboard/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,38 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Icon, Stack } from '@chakra-ui/react'
 import React from 'react'
+import { PiSquaresFourFill } from "react-icons/pi";
 
 function Dashboard() {
+
+    const sideNavLinks=[
+        {
+            icon: PiSquaresFourFill,
+            title: 'Dashboard',
+
+        },
+        {
+            icon: PiSquaresFourFill,
+            title: 'Transaction',
+
+        },
+
+    ]
   return (
     <Box >
-      Dashboard
+        <Stack>
+            {sideNavLinks.map((nav)=>{
+                return(
+                    <HStack>
+                        <Icon as={nav.icon} />
+                        
+                            <div>
+                                {nav.title}
+                            </div>
+                        
+                    </HStack>
+                )
+            })}
+        </Stack>
     </Box>
   )
 }
