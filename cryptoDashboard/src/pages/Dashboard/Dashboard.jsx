5ced60dc05ffc72e7c7dca0088ae78471f0328a3
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-09-04 03:11:12 +0530

    chore: Update Dashboard page layout and add support icon

--- cryptoDashboard/src/pages/Dashboard/Dashboard.jsx
+++ cryptoDashboard/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
 import { Box, HStack, Icon, Stack } from '@chakra-ui/react'
 import React from 'react'
-import { PiSquaresFourFill } from "react-icons/pi";
-
+import { PiSquaresFourFill} from "react-icons/pi";
+import { TbArrowsDoubleNwSe } from "react-icons/tb";
+import { BiSupport } from "react-icons/bi";
 function Dashboard() {
 
     const sideNavLinks=[
@@ -11,27 +12,45 @@ function Dashboard() {
 
         },
         {
-            icon: PiSquaresFourFill,
+            icon: TbArrowsDoubleNwSe,
             title: 'Transaction',
 
         },
-
     ]
   return (
     <Box >
-        <Stack>
-            {sideNavLinks.map((nav)=>{
-                return(
-                    <HStack>
-                        <Icon as={nav.icon} />
-                        
-                            <div>
-                                {nav.title}
-                            </div>
-                        
-                    </HStack>
-                )
-            })}
+        <Stack className='flex justify-between  max-w-64 h-screen '>
+            <Box>
+                <h1 className='text-xl pt-16 font-bold text-center'>Crypto Dashboard</h1>
+                <Box className='mt-6 mx-3'>
+                    {sideNavLinks.map((nav)=>{
+                        return(
+                            <HStack key={nav.title} className='py-3 px-4 rounded-md hover:bg-gray-200 hover:text-gray-900'>
+                                <Icon as={nav.icon} />
+                                
+                                    <p>
+                                        {nav.title}
+                                    </p>
+                                
+                            </HStack>
+                            
+                        )
+                    }
+                    )}
+                
+                </Box>
+            </Box>
+            <Box className='mt-6 mx-3' >
+                <HStack className='py-3 px-4 hover:bg-gray-200 hover:text-gray-900 rounded-md'>
+                        <Icon as={BiSupport} />
+                            
+                        <p>
+                            Support
+                        </p>
+                            
+                </HStack>
+            </Box>
+            
         </Stack>
     </Box>
   )
