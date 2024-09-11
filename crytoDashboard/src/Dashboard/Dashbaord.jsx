53b0ddb80eb365bb824a6189bb742f87b6de8471
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-09-11 19:07:57 +0530

    Refactor Dashboard component, replace SideNav with Layout component

--- crytoDashboard/src/Dashboard/Dashbaord.jsx
+++ crytoDashboard/src/Dashboard/Dashbaord.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SideNav from '../components/SideNav'
+import Layout from '../components/DashBoardLayout/Layout'
 
 function Dashbaord() {
   return (
     <div>
-        <SideNav />
+        <Layout />
     </div>
   )
 }
--- crytoDashboard/src/components/TopNav.jsx
+++ crytoDashboard/src/components/TopNav.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { MdAccountCircle } from "react-icons/md";
-<<<<<<< HEAD
+
 import { TfiMenuAlt } from "react-icons/tfi";
-=======
->>>>>>> 03595d2064d9470f0681b9a9fc1a6937861ff0f5
+
 
 
 function TopNav() {
@@ -29,53 +28,41 @@ function TopNav() {
         setToggle(!toggle)
     }
   return (
-<<<<<<< HEAD
+
     <div className='w-screen bg-gray-300 h-16'>
-        <div
-        className='
-        pt-7
-        '
-        >
-            <TfiMenuAlt />
-        </div>
-        <div
-        className='flex justify-between items-center h-0 pb-3 mx-36 '
+        
+        <div className='flex justify-between items-center h-0 pb-3 mx-36 '>
 
-    <div
-    className='w-screen bg-gray-300 h-16'
-    >
-        <div 
-        className='flex justify-between items-center p-4 mx-36'
+        <div className='w-screen bg-gray-300 h-16'>
+        
 
-        >
-            <h1
-            className='text-2xl font-bold  
-            '
-            >Dashboard</h1>
-            <button
-            className=''
-            >
-                <MdAccountCircle 
-                className='text-3xl'
-                onClick={handleToggle}
-                />
-                {
-                    toggle && (
-                        <div
-                        className='absolute top-16 right-32 bg-gray-300'
-                        >
-                            {toggleLinks.map((link, index) => (
-                                <ul key={index}>
-                                    <li className='p-2'>{link.title}</li>
-                                </ul>
-                            ))}
-                        </div>
-                    )
-                }
-            </button>
+
+            <div className='flex justify-between items-center p-4 mx-36'>
+                <h1 className='text-2xl font-bold '>Dashboard</h1>
+                <button className=''>
+                    <MdAccountCircle 
+                    className='text-3xl'
+                    onClick={handleToggle}
+                    />
+                    {
+                        toggle && (
+                            <div className='absolute top-16 right-32 bg-gray-300'>
+                                {toggleLinks.map((link, index) => (
+                                    <ul key={index}>
+                                        <li className='p-2'>{link.title}</li>
+                                    </ul>
+                                ))}
+                            </div>
+                        )
+                    }
+                </button>
+            </div>
         </div>
+        
     </div>
-  )
+    </div>
+  ) 
+
 }
 
 export default TopNav
